458befd782e25ea6f5d115588aa4b27b894aa77c
LuchkinPI91 <luchkin-nikita@list.ru>
2023-02-23 19:41:44 +0700

    Создан метод о уведомлении о дне рождения друга

--- MemoAppTests.py
+++ MemoAppTests.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 from My_App import MemoApp
 
@@ -19,6 +20,19 @@ class MemoAppTests(unittest.TestCase):
         memoApp.delete_friend_birthday('Alex', '20.05.2001')
         self.assertEqual(memoApp.birth_day_dic["birthday"], ['20.05.2001', '20.05.2001'])
         self.assertEqual(memoApp.birth_day_dic["friend_name"], ['Alex1', 'Alex2'])
+
+    def test_birthday_reminder(self):
+        memoApp = MemoApp.Memo_App()
+
+        date_now = datetime.datetime.now()
+        date_now = date_now.strftime("%d.%m.%Y")
+        memoApp.add_friend_birthday('Alex', date_now)
+        memoApp.add_friend_birthday('Alex1', date_now)
+        memoApp.add_friend_birthday('Alex2', '23.02.2001')
+
+        status, friend_name = memoApp.birthday_reminder()
+        self.assertEqual(friend_name, ['Alex', 'Alex1'])
+        self.assertTrue(status)
 
 if __name__=="__main__":
     unittest.main()
--- My_App/MemoApp.py
+++ My_App/MemoApp.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 
 
@@ -26,6 +27,29 @@ class Memo_App:
             if i == birthday and friends_name[counter] == friend_name:
                 birthdays.pop(counter)
                 friends_name.pop(counter)
-            counter+=1
+            counter += 1
         self.birth_day_dic["birthday"] = birthdays
         self.birth_day_dic["friend_name"] = friends_name
+
+    def birthday_reminder(self):
+        date_now = datetime.datetime.now()
+
+        date_now = date_now.strftime("%d.%m.%Y")
+
+
+
+        birthdays = self.birth_day_dic["birthday"]
+        friends_name = self.birth_day_dic["friend_name"]
+        counter = 0
+
+        friends_list = list()
+
+        for i in birthdays:
+
+            if i == date_now:
+                friends_list.append(friends_name[counter])
+
+            counter += 1
+        if len(friends_list) > 0:
+            return True, friends_list
+        return False, friends_list
