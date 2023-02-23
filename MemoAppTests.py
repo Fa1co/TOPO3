9b0a7e898a45d1fdc54edac8c45de5486c406faf
LuchkinPI91 <luchkin-nikita@list.ru>
2023-02-23 19:17:07 +0700

    Создан метод удаления дня рождения друга

--- MemoAppTests.py
+++ MemoAppTests.py
@@ -10,5 +10,15 @@ class MemoAppTests(unittest.TestCase):
         self.assertEqual(memoApp.birth_day_dic["friend_name"][0], 'Alex')
 
 
+    def test_delete_friend_birthday(self):
+        memoApp = MemoApp.Memo_App()
+        memoApp.add_friend_birthday('Alex', '20.05.2001')
+        memoApp.add_friend_birthday('Alex1', '20.05.2001')
+        memoApp.add_friend_birthday('Alex2', '20.05.2001')
+        memoApp.add_friend_birthday('Alex', '20.05.2001')
+        memoApp.delete_friend_birthday('Alex', '20.05.2001')
+        self.assertEqual(memoApp.birth_day_dic["birthday"], ['20.05.2001', '20.05.2001'])
+        self.assertEqual(memoApp.birth_day_dic["friend_name"], ['Alex1', 'Alex2'])
+
 if __name__=="__main__":
     unittest.main()
--- My_App/MemoApp.py
+++ My_App/MemoApp.py
@@ -16,3 +16,16 @@ class Memo_App:
         friends_name = self.birth_day_dic["friend_name"]
         friends_name.append(friend_name)
         self.birth_day_dic["friend_name"] = friends_name
+
+    def delete_friend_birthday(self, friend_name, birthday):
+        birthdays = self.birth_day_dic["birthday"]
+        friends_name = self.birth_day_dic["friend_name"]
+        counter = 0
+
+        for i in birthdays:
+            if i == birthday and friends_name[counter] == friend_name:
+                birthdays.pop(counter)
+                friends_name.pop(counter)
+            counter+=1
+        self.birth_day_dic["birthday"] = birthdays
+        self.birth_day_dic["friend_name"] = friends_name
